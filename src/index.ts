5da20505105fb6a1c0c4e2564bfa6e692b355ffd
ruchiayeon <ruchia10326@gmail.com>
2019-04-20 17:22:25 +0900

    database connected

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Options } from "graphql-yoga";
 import app from "./app";
+import {createConnection} from "typeorm";
+import connectionOtions from "./ormConfig";
 
-
-const PORT: number | string = process.env.PORT || 4000;
+const PORT: number | string = process.env.PORT || 3000;
 const PLAYGROUND_ENDPOINT : string = "/playground";
 const GRAPHQL_ENDPOINT: string = "/graphql";
 
@@ -14,6 +15,10 @@ const appOptions: Options ={
 
 const handleAppStart = () => console.log(`Port open ${PORT}`);
 
-app.start(appOptions,handleAppStart); // app.start 함수는 콜백함수도 받을 수 있다. AND options의 파라미터와 콜백 파라미터를 가진다. 
+createConnection(connectionOtions).then(()=>{
+    app.start(appOptions,handleAppStart); 
+})// app.start 함수는 콜백함수도 받을 수 있다. AND options의 파라미터와 콜백 파라미터를 가진다. 
 
 //콜백에는 물음표가 붙어있다. == 콜백함수가 있어도되고 없어도 된다는의미임.
+
+//추가로 데이터 베이스를 연결을 먼저 한뒤에 앱을 실행하도록 함. `
